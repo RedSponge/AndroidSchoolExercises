1784091f04cd1ee54091edb3ea060b1a07136fba
StudentN <studentN@branco-ph.local>
2020-01-21 15:41:48 +0200

    Start adding shared pref

--- app/src/main/java/com/redsponge/coolapp/projects/CookieClickerActivity.java
+++ app/src/main/java/com/redsponge/coolapp/projects/CookieClickerActivity.java
@@ -1,11 +1,13 @@
 package com.redsponge.coolapp.projects;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.coolapp.R;
+import com.redsponge.coolapp.util.Constants;
 
 public class CookieClickerActivity extends Activity {
 
@@ -18,6 +20,8 @@ public class CookieClickerActivity extends Activity {
         setContentView(R.layout.activity_cookie_clicker);
 
 
+        SharedPreferences sharedPref = getSharedPreferences(Constants.SharedPreferencePaths.COOKIE_CLICKER_COUNT, MODE_PRIVATE);
+        cookieCount = sharedPref.getInt(Constants.SharedPreferencePaths.COOKIE_CLICKER_COUNT, 0);
         tvCounter = findViewById(R.id.tvCounter);
         cookieCount = 0;
         updateCookieDisplay();
@@ -31,5 +35,10 @@ public class CookieClickerActivity extends Activity {
 
     private void updateCookieDisplay() {
         tvCounter.setText(getString(R.string.cookie_clicker_counter_display, cookieCount));
+    }
+
+    public void saveCookies(View view) {
+        SharedPreferences sharedPref = getSharedPreferences(Constants.SharedPreferencePaths.COOKIE_CLICKER_COUNT, MODE_PRIVATE);
+        sharedPref.edit().putInt(Constants.SharedPreferencePaths.COOKIE_CLICKER_COUNT, cookieCount).apply();
     }
 }
---
+++ app/src/main/java/com/redsponge/coolapp/util/Constants.java
@@ -0,0 +1,12 @@
+package com.redsponge.coolapp.util;
+
+public class Constants {
+
+    public static final class SharedPreferencePaths {
+
+        public static final String SHARED_PREFERENCES = "RedSpongeAppPreferences";
+
+        public static final String COOKIE_CLICKER_COUNT = "CookieClickerCount";
+    }
+
+}
