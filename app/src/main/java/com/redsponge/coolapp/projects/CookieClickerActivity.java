33918bd1faa01f313cf4aa26372a5cb5f70b1b85
RedSponge <eran.godsi2003@gmail.com>
2020-01-25 21:35:15 +0200

    Fix cookieclicker load bug and change picture

--- app/src/main/java/com/redsponge/coolapp/projects/CookieClickerActivity.java
+++ app/src/main/java/com/redsponge/coolapp/projects/CookieClickerActivity.java
@@ -1,15 +1,19 @@
 package com.redsponge.coolapp.projects;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.coolapp.R;
-import com.redsponge.coolapp.util.Constants;
+import com.redsponge.coolapp.util.alert.AlertUtils;
 
 public class CookieClickerActivity extends Activity {
+
+    private static final String TAG = "CookieClickerActivity";
 
     private TextView tvCounter;
     private int cookieCount;
@@ -19,12 +23,22 @@ public class CookieClickerActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cookie_clicker);
 
-
-        SharedPreferences sharedPref = getSharedPreferences(Constants.SharedPreferencePaths.COOKIE_CLICKER_COUNT, MODE_PRIVATE);
-        cookieCount = sharedPref.getInt(Constants.SharedPreferencePaths.COOKIE_CLICKER_COUNT, 0);
         tvCounter = findViewById(R.id.tvCounter);
-        cookieCount = 0;
+
+        /*SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_preferences_name), MODE_PRIVATE);
+        int storedCookieCount = sharedPref.getInt(getString(R.string.shared_preferences_cookie_clicker_cookie_count), 0);
         updateCookieDisplay();
+        if(storedCookieCount != 0) {
+            AlertUtils.showConfirmPrompt(this, "Found Saved Cookies!",
+                    "After searching throuroughly in the cabinet we found " + storedCookieCount + " cookies! Load them? (no means losing them forever!)",
+                    (dialog, which) ->{
+                        cookieCount = storedCookieCount;
+                        updateCookieDisplay();
+                    },
+                    (dialog, which) -> {
+                        sharedPref.edit().putInt(getString(R.string.shared_preferences_cookie_clicker_cookie_count), 0).apply();
+                    });
+        }*/
     }
 
 
@@ -38,7 +52,7 @@ public class CookieClickerActivity extends Activity {
     }
 
     public void saveCookies(View view) {
-        SharedPreferences sharedPref = getSharedPreferences(Constants.SharedPreferencePaths.COOKIE_CLICKER_COUNT, MODE_PRIVATE);
-        sharedPref.edit().putInt(Constants.SharedPreferencePaths.COOKIE_CLICKER_COUNT, cookieCount).apply();
+        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.shared_preferences_name), Context.MODE_PRIVATE);
+        sharedPref.edit().putInt(getString(R.string.shared_preferences_cookie_clicker_cookie_count), cookieCount).apply();
     }
 }
